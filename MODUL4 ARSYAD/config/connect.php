dd291b2c2e500a43e269992cbd4c42437c7396ce
arsyadal <116419335+arsyadal@users.noreply.github.com>
2023-11-23 18:40:27 +0700

    connect.php

--- MODUL4 ARSYAD/config/connect.php
+++ MODUL4 ARSYAD/config/connect.php
@@ -2,7 +2,7 @@
 <?php 
 
 // (1) Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
-$db = mysqli_connect('localhost', 'root', '', modul4) 
+$db = mysqli_connect('localhost', 'root', '', 'coba'); // Add a semicolon at the end
 // 
 
 // (2) Buatlah perkondisian untuk menampilkan pesan error ketika database gagal terkoneksi
